da92be9d79a745aac95230e9061d97b3238d585f
Jake <jacob.knitter@live.com>
2015-07-09 00:21:38 -0400

    fix define

--- file/video-util.js
+++ file/video-util.js
@@ -1,20 +1,20 @@
 'use strict';
-
-exports.mimeTypes = {
-	"swf": "application/x-shockwave-flash",
-	"flv": "video/x-flv",
-	"mpeg": "video/mpeg",
-	"mpg": "video/mpeg",
-	"mpv2": "video/mpeg",
-	"mp2": "video/mpeg",
-	"ogg": "video/ogg"
-};
-
-exports.getExt = function(name){
-	return name.split('.').pop();
-};
-
-exports.getExtMime = function(ext){
-	return mimeTypes[ext] || 'video/mp4';
-};
-
+define(function(){
+	return { 
+		mimeTypes:{
+			"swf": "application/x-shockwave-flash",
+			"flv": "video/x-flv",
+			"mpeg": "video/mpeg",
+			"mpg": "video/mpeg",
+			"mpv2": "video/mpeg",
+			"mp2": "video/mpeg",
+			"ogg": "video/ogg"
+		},
+		getExt: function(name){
+			return name.split('.').pop();
+		},
+		getExtMime: function(ext){
+			return mimeTypes[ext] || 'video/mp4';
+		}
+	}
+});
